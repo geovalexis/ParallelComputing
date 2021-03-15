be90481afc29148e9c5d0c802a40039ba40f0a71
Geovanny Risco <geovannyalexanderr@gmail.com>
2021-03-15 22:29:22 +0100

    Split script

---
+++ p1.1/kmeanCPM_secuencial.c
@@ -0,0 +1,119 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+#define N 600000 //muestras
+#define G 200	 //centroides
+
+long V[N];
+long R[G];
+int A[G];
+
+void kmean(int fN, int fK, long fV[], long fR[], int fA[])
+{
+	int i, j, min, iter = 0;
+	long dif, t;
+	long fS[G];
+	int fD[N];
+
+	do
+	{
+		for (i = 0; i < fN; i++)
+		{
+			min = 0;
+			dif = abs(fV[i] - fR[0]);
+			for (j = 1; j < fK; j++)
+				if (abs(fV[i] - fR[j]) < dif)
+				{
+					min = j;
+					dif = abs(fV[i] - fR[j]);
+				}
+			fD[i] = min;
+		}
+
+		for (i = 0; i < fK; i++)
+			fS[i] = fA[i] = 0;
+
+		for (i = 0; i < fN; i++)
+		{
+			fS[fD[i]] += fV[i];
+			fA[fD[i]]++;
+		}
+
+		dif = 0;
+		for (i = 0; i < fK; i++)
+		{
+			t = fR[i];
+			if (fA[i])
+				fR[i] = fS[i] / fA[i];
+			dif += abs(t - fR[i]);
+		}
+		iter++;
+	} while (dif);
+
+	printf("iter %d\n", iter);
+}
+
+void qs(int ii, int fi, long fV[], int fA[])
+{
+	int i, f, j;
+	long pi, pa, vtmp, vta, vfi, vfa;
+
+	pi = fV[ii];
+	pa = fA[ii];
+	i = ii + 1;
+	f = fi;
+	vtmp = fV[i];
+	vta = fA[i];
+
+	while (i <= f)
+	{
+		if (vtmp < pi)
+		{
+			fV[i - 1] = vtmp;
+			fA[i - 1] = vta;
+			i++;
+			vtmp = fV[i];
+			vta = fA[i];
+		}
+		else
+		{
+			vfi = fV[f];
+			vfa = fA[f];
+			fV[f] = vtmp;
+			fA[f] = vta;
+			f--;
+			vtmp = vfi;
+			vta = vfa;
+		}
+	}
+	fV[i - 1] = pi;
+	fA[i - 1] = pa;
+
+	if (ii < f)
+		qs(ii, f, fV, fA);
+	if (i < fi)
+		qs(i, fi, fV, fA);
+}
+
+int main()
+{
+	int i;
+
+	// Inicialización
+	for (i = 0; i < N; i++)
+		V[i] = (rand() % rand()) / N;
+
+	// primers candidats
+	for (i = 0; i < G; i++)
+		R[i] = V[i];
+
+	// calcular els G mes representatius
+	kmean(N, G, V, R, A);
+
+	qs(0, G - 1, R, A);
+
+	for (i = 0; i < G; i++)
+		printf("R[%d] : %ld te %d agrupats\n", i, R[i], A[i]);
+
+	return (0);
+}
